c18ed4582ab7f6daeb462ac1b55edec177659386
NCC_TOKIYA_MINAGAWA <melon2560@users.noreply.github.com>
2024-12-05 22:32:41 +0900

    Pythonを書く練習をしてみた。

---
+++ __pycache__/20241205.py
@@ -0,0 +1,43 @@
+# this is the first comment
+spam = 1    # and this is the second comment
+            # and this is the third!
+text = "# This is not a comment because it's inside quotes."
+
+print(2 + 2)
+print(50 - 5 * 6)
+print((50 - 5 * 6) / 4)
+print(8 / 5)
+
+print(17 / 3)
+print(17 // 3)
+print(17 % 3)
+print(5 * 3 + 2)
+
+print(5 ** 2)
+print(2 ** 7)
+
+width = 20
+height = 5 * 9
+print(width * height)
+
+n = ""
+print(n)
+
+print(4 * 3.75 - 1)
+
+tax = 12.5 / 100
+price = 100.50
+print(price * tax)
+
+print(price + (price * tax))
+print(round((price + (price * tax)), 2))
+
+print('spam eggs')
+print("Paris rabbit got your back:)! Yay!")
+print('1975')
+
+print('doesn\'t')
+print("doesn't")
+print('"YES," they said.' )
+print("\"YES,\" they said.")
+print('"Isn\'t," they said')
