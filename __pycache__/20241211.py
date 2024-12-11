51d50f2c0ad1a509594dc658006a33cf94618d03
NCC_TOKIYA_MINAGAWA <melon2560@users.noreply.github.com>
2024-12-11 21:54:35 +0900

    20241211の学習の記録

---
+++ __pycache__/20241211.py
@@ -0,0 +1,86 @@
+for i in range(5):
+    print(i)
+
+print(list(range(5, 10)))
+print(list(range(0, 10, 3)))
+print(list(range(-10, -100, -30)))
+
+a = ['Mary', 'had', 'a', 'little', 'lamb']
+for i in range(len(a)):
+    print(i, a[i])
+
+print(range(10))
+
+print(sum(range(4)))    # 0 + 1 + 2 + 3
+
+for num in range(2, 10):
+    if num % 2 == 0:
+        print(f"Found an even number {num}")
+        continue
+    print(f"Found an odd number {num}")
+
+for n in range(2, 10):
+    for x in range(2, n):
+        if n % x == 0:
+            print(n, 'equals', x, '*', n//x)
+            break
+    else:
+        # loop fell through without finding a factor
+        print(n, 'is a prime number')
+
+# while True:
+#    pass    # Busy-wait for keyboard interrupt (Ctlr+C)
+
+class MyEmptyClass:
+    pass
+
+def initlog(args):
+    pass    # Remember to implement this!
+
+def http_error(status):
+    match status:
+        case 400:
+            return "Bad request"
+        case 404:
+            return "Not found"
+        case 418:
+            return "I'm a teapot"
+        case 401 | 403 | 404:
+            return "Not allowed"
+        case _:
+            return "Something's wrong with the internet"
+        
+# pointタプルを定義
+point = (1, 2)    # 例として座標(1, 2)を使用
+
+# point is an (x, y) tuple
+match point:
+    case (0, 0):
+        print("Origin")
+    case (0, y):
+        print(f"Y={y}")
+    case (x, 0):
+        print(f"X={x}")
+    case (x, y):
+        print(f"X={x}, Y={y}")
+    case _:
+        raise ValueError("Not a point")
+
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+def where_is(point):
+    match point:
+        case Point(x=0, y=0):
+            print("Origin")
+        case Point(x=0, y=y):
+            print(f"Y={y}")
+        case Point(x=x, y=0):
+            print(f"X={x}")
+        case Point():
+            print("Somewhere else")
+        case _:
+            print("Not a point")
+
